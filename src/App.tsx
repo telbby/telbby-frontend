299301756bbe63849650ac9c521804a9cbac3537
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-19 09:14:08 +0900

    Add emotion for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from 'react';
 import './App.css';
+import { css } from '@emotion/react';
 import testImage from './assets/images/test.jpg';
 
 const App: FC = () => {
   return (
     <>
       <h1>Hello World {process.env.APP_ENV}</h1>
-      <h2>Hello Hello</h2>
+      <h2
+        css={css`
+          color: red;
+        `}
+      >
+        Hello Hello
+      </h2>
       <img src={testImage} alt="test" width="500px" />
     </>
   );
