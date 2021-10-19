abf78e358645bc2c06228ad82594d87e2e4d2a70
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-19 16:18:40 +0900

    Build a basic directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React, { FC } from 'react';
 import './App.css';
 import { css } from '@emotion/react';
-import testImage from './assets/images/test.jpg';
 
 const App: FC = () => {
   return (
-    <>
-      <h1>Hello World {process.env.APP_ENV}</h1>
-      <h2
-        css={css`
-          color: red;
-        `}
-      >
-        Hello Hello
-      </h2>
-      <img src={testImage} alt="test" width="500px" />
-    </>
+    <h1
+      css={css`
+        color: red;
+      `}
+    >
+      Hello Hello
+    </h1>
   );
 };
 
---
+++ src/constants/uri.ts
@@ -0,0 +1,7 @@
+enum Uri {
+  main = '/',
+  login = '/login',
+  signup = '/signup',
+}
+
+export default Uri;
