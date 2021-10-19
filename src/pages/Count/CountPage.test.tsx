dd7899d43116f59b7cde40f72b3238a117c45a0a
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-19 17:36:38 +0900

    Fix test error

--- src/pages/Count/CountPage.test.tsx
+++ src/pages/Count/CountPage.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { render, screen } from '@testing-library/react';
 
 import CountPage from './CountPage';
 
 it('should render "Count"', () => {
-  render(<CountPage />);
+  render(
+    <RecoilRoot>
+      <CountPage />
+    </RecoilRoot>,
+  );
   screen.getByText('Count');
 });
