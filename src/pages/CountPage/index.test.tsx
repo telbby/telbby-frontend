0b356d9c99952bbba5ec50f6d1782149574b15f4
Seogeurim <geulims@naver.com>
2021-11-01 02:09:03 +0900

    [#3] test: test 코드 수정

--- src/pages/CountPage/index.test.tsx
+++ src/pages/CountPage/index.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import { render, screen } from '@testing-library/react';
@@ -7,9 +8,11 @@ import CountPage from '.';
 
 it('should render "Count"', () => {
   render(
-    <RecoilRoot>
-      <CountPage />
-    </RecoilRoot>,
+    <BrowserRouter>
+      <RecoilRoot>
+        <CountPage />
+      </RecoilRoot>
+    </BrowserRouter>,
   );
   screen.getByText('Count');
 });
--- src/pages/HomePage/index.test.tsx
+++ src/pages/HomePage/index.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
 
 import HomePage from '.';
 
 it('should render "Home"', () => {
-  render(<HomePage />);
-  screen.getByText('Home');
+  render(
+    <BrowserRouter>
+      <HomePage />
+    </BrowserRouter>,
+  );
+  screen.getByText('Talk to telbby');
 });
