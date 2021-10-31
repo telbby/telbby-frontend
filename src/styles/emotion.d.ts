f0c932207d892ef27b418df6a85cf85dcdb6488f
Seogeurim <geulims@naver.com>
2021-11-01 01:21:33 +0900

    [#3] style: theme color 변수 생성

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -4,5 +4,14 @@ declare module '@emotion/react' {
   export interface Theme {
     colorPrimary: string;
     colorPrimaryDark: string;
+    colorSub: string;
+    colorBg: string;
+    colorWhite: string;
+    colorGray1: string;
+    colorGray2: string;
+    colorGray3: string;
+    colorBrown: string;
+    colorSuccess: string;
+    colorError: string;
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,15 @@
 const theme = {
   colorPrimary: '#4A7AFF',
   colorPrimaryDark: '#0053BF',
+  colorSub: 'F0C630',
+  colorBg: '#F4F7FF',
+  colorWhite: '#FFFFFF',
+  colorGray1: '#7F8086',
+  colorGray2: '#9196AA',
+  colorGray3: '#C4C4C4',
+  colorBrown: '#554545',
+  colorSuccess: '#6DCCA3',
+  colorError: '#F45452',
 };
 
 export default theme;
