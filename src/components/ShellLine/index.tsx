243897231398a1d4552db742758d76989a9c335c
박종호 <jong951005@gmail.com>
2021-11-01 17:29:01 +0900

    [#9] feat: 폼 클릭 시 마지막 input에 focus 주도록 컨트롤 하는 함수 추가

--- src/components/ShellLine/index.tsx
+++ src/components/ShellLine/index.tsx
@@ -1,12 +1,24 @@
 import React, { ReactElement } from 'react';
 
-const ShellLine = ({ type, title }): ReactElement => {
+type ShellLineProps = {
+  type: string;
+  title: string;
+};
+
+const ShellLine = ({
+  type, //
+  title,
+}: ShellLineProps): ReactElement => {
   return (
     <label htmlFor={title}>
       {type} {title} :
       <input type="text" id={title} />
     </label>
   );
+};
+
+ShellLine.defaultProps = {
+  // autofocus: false,
 };
 
 export default ShellLine;
