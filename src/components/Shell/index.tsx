1a82b78160ee489e9c7fb73ac6a9dc24900894f1
박종호 <jong951005@gmail.com>
2021-11-01 17:35:55 +0900

    [#9] feat: 폼 클릭 시 마지막 input에 focus 주도록 컨트롤 하는 함수 추가

--- src/components/Shell/index.tsx
+++ src/components/Shell/index.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useRef } from 'react';
 
 import { SHELL_SUCCESS_MESSAGE, SHELL_TITLE } from '@/constants/shell';
 import useShell from '@/hooks/useShell';
@@ -12,25 +11,34 @@ type ShellProps = {
 /**
  * Shell 형태의 Chat UI 컴포넌트 입니다.
  * form 리스트를 넘겨줘야 합니다.
- *
- * input 요소에 keyDown event를 넘겨줘야 하는데,
- * 이를 일일히 바인딩 하기보단 fieldset에 붙여줘 아래 요소에 전파되도록 하였습니다.
- * 이 때, noninteractive 요소에 interaction을 붙이면 안된다는 linter 에러가 있어 잠시 꺼두었습니다.
- * 추후에 리팩토링하며 수정할 계획이 있습니다.
  */
 const Shell = ({ formElement, subTitle }: ShellProps): ReactElement => {
   const { shellLines, setNextShellLine } = useShell(formElement);
+  const fieldsetRef = useRef<HTMLFieldSetElement>();
+
+  const checkFormValueValidation = (e) => {
+    if (e.key === 'Enter') setNextShellLine();
+  };
+
+  const handleFocus = () => {
+    const lines = fieldsetRef.current.querySelectorAll('label');
+    lines[lines.length - 1].focus();
+  };
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form
+      aria-hidden
+      onClick={handleFocus}
+      onSubmit={(e) => e.preventDefault()}
+    >
       <h1>
         {SHELL_TITLE}
         {subTitle && ` - ${subTitle}`}
       </h1>
       <fieldset
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') setNextShellLine();
-        }}
+        aria-hidden
+        onKeyDown={checkFormValueValidation}
+        ref={fieldsetRef}
       >
         <legend>Telbby Service Terminal: </legend>
         {shellLines}
--- src/components/ShellLine/index.tsx
+++ src/components/ShellLine/index.tsx
@@ -1,24 +1,12 @@
 import React, { ReactElement } from 'react';
 
-type ShellLineProps = {
-  type: string;
-  title: string;
-};
-
-const ShellLine = ({
-  type, //
-  title,
-}: ShellLineProps): ReactElement => {
+const ShellLine = ({ type, title }): ReactElement => {
   return (
     <label htmlFor={title}>
       {type} {title} :
       <input type="text" id={title} />
     </label>
   );
-};
-
-ShellLine.defaultProps = {
-  // autofocus: false,
 };
 
 export default ShellLine;
