a19b421a135554cfc74600074588b24a5aa2982c
Seogeurim <geulims@naver.com>
2021-11-02 18:34:13 +0900

    [#4] test: Jumbotron test 추가

---
+++ src/components/common/Jumbotron/index.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import Jumbotron from '.';
+
+it('Jumbotron should render title & desc text received as props', () => {
+  render(<Jumbotron title="Test" desc={['desciption test']} />);
+  expect(screen.getByRole('heading')).toHaveTextContent('Test to telbby');
+  expect(screen.queryByText('desciption test')).toBeInTheDocument();
+});
