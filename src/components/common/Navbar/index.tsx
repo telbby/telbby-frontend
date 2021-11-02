30ef843157fb8c91ef470b42e46638e34c796fac
Seogeurim <geulims@naver.com>
2021-11-02 15:05:47 +0900

    [#4] refactor: Logo 컴포넌트 common으로 분류

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 
-import Logo from '@/components/Logo';
+import Logo from '../Logo';
 import Uri from '@/constants/uri';
 
 import { navStyle, yellowLineButton, yellowFullButton } from './style';
