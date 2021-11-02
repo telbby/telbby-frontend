19617397cca08cd9a102bfdd7d68fbc3996d76eb
박종호 <jong951005@gmail.com>
2021-11-02 14:05:40 +0900

    [#9] chore: formElement 상수로 관리하도록 수정

--- src/constants/shell.ts
+++ src/constants/shell.ts
@@ -2,4 +2,38 @@ export const SHELL_TITLE = 'telbby init v0.1.0';
 export const SHELL_SUCCESS_MESSAGE =
   'Congrats! service has been added to telbby.';
 
-export const SHELL_ERROR_ACCESS_DENIED = 'Access denied for user';
+export const SHELL_ERROR_USER_ACCESS_DENIED = 'Access denied';
+
+export const SHELL_LOGIN_FORM_ELEMENT = [
+  { lineType: 'question', lineTitle: 'username', bodyKey: 'username' },
+  {
+    lineType: 'question',
+    lineTitle: 'password',
+    inputType: 'password',
+    bodyKey: 'password',
+  },
+  {
+    lineType: 'question',
+    lineTitle: 'Sign in? [y/n]',
+    checkValidation: (val: string): boolean => {
+      if (!['y', 'n'].includes(val)) return false;
+      return true;
+    },
+  },
+];
+
+export const SHELL_FEEDBACK_FORM_ELEMENT = [
+  { lineType: 'config', lineTitle: 'service name', bodyKey: 'name' },
+  {
+    lineType: 'config',
+    lineTitle: 'service description',
+    bodyKey: 'description',
+  },
+  { lineType: 'config', lineTitle: 'service domain', bodyKey: 'domain' },
+  { lineType: 'config', lineTitle: 'color theme', bodyKey: 'color-theme' },
+  {
+    lineType: 'config',
+    lineTitle: 'first question',
+    bodyKey: 'first-question',
+  },
+];
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,30 +2,16 @@ import React, { FC } from 'react';
 
 import Navigation from '@/components/Navigation';
 import Shell from '@/components/Shell';
+import { SHELL_LOGIN_FORM_ELEMENT } from '@/constants/shell';
 
 import { titleStyle } from './style';
 
-const HomePage: FC = () => {
-  const formElement = [
-    { lineType: 'question', lineTitle: 'username' },
-    { lineType: 'question', lineTitle: 'password', inputType: 'password' },
-    {
-      lineType: 'question',
-      lineTitle: 'Sign in? [y/n]',
-      checkValidation: (val: string) => {
-        if (!['y', 'n'].includes(val)) return false;
-        return true;
-      },
-    },
-  ];
-
-  return (
-    <div>
-      <Navigation />
-      <h1 css={titleStyle}>Talk to telbby</h1>
-      <Shell formElement={formElement} />
-    </div>
-  );
-};
+const HomePage: FC = () => (
+  <div>
+    <Navigation />
+    <h1 css={titleStyle}>Talk to telbby</h1>
+    <Shell formElement={SHELL_LOGIN_FORM_ELEMENT} subTitle="service settings" />
+  </div>
+);
 
 export default HomePage;
