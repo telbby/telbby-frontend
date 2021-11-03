a6d38166ab8bdf7ad32317772c57bd16c863cc50
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-03 11:35:32 +0900

    [#10] refactor: useSignup hook에서 사용자 정보 저장 로직 삭제

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -4,41 +4,38 @@ import { useHistory } from 'react-router-dom';
 import { LoginRequestBody } from '@/apis/types';
 import * as usersApi from '@/apis/users';
 import { signupError } from '@/constants/error';
-
-import useAuth from './useAuth';
+import Uri from '@/constants/uri';
 
 const useSignup = (): {
   signup: ({ id, password }: LoginRequestBody) => Promise<void>;
-  loading: boolean;
+  isLoading: boolean;
   error: string;
 } => {
-  const { authorize } = useAuth();
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const history = useHistory();
 
   const signup = async ({ id, password }: LoginRequestBody) => {
     try {
-      setLoading(true);
+      setIsLoading(true);
 
-      const { idx } = await usersApi.signup({ id, password });
-      const user = { idx, id };
+      await usersApi.signup({ id, password });
 
-      authorize(user);
-      history.replace('/');
+      // @TODO 회원가입 성공 시 로그인 페이지로 이동
+      history.replace(Uri.home);
     } catch (e) {
       if (signupError[e.response.status]) {
         setError(signupError[e.response.status]);
         throw e;
       }
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
   };
 
   return {
     signup,
-    loading,
+    isLoading,
     error,
   };
 };
