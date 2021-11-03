c9f549c6a45aecbff0091d82cb37f15ba0e9b1f2
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-03 10:20:31 +0900

    [#10] refactor: useAuth hook에 대해서 authorize 대신 login 함수 추가

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,3 +1,7 @@
 export const signupError = {
   409: 'User already exists',
 };
+
+export const loginError = {
+  401: 'Please check your ID or password again.',
+};
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,41 @@
-import * as authApi from '@/apis/auth';
-import { User } from '@/apis/types';
-import { useUserState } from '@/atoms/userState';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const useAuth = (): { authorize: (user: User) => void; logout: () => void } => {
+import * as authApi from '@/apis/auth';
+import { LoginRequestBody } from '@/apis/types';
+import { useUserState } from '@/atoms/userState';
+import { loginError } from '@/constants/error';
+import Uri from '@/constants/uri';
+
+const useAuth = (): {
+  login: ({ id, password }: LoginRequestBody) => Promise<void>;
+  logout: () => void;
+  isLoading: boolean;
+  error: string;
+} => {
   const [, setUserState] = useUserState();
-  const authorize = (user: User) => setUserState(user);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const history = useHistory();
+
+  const login = async ({ id, password }: LoginRequestBody) => {
+    try {
+      setIsLoading(true);
+
+      await authApi.login({ id, password });
+
+      // @TODO 로그인 성공 시 사용자 정보 가져오는 API 호출 후 상태로 저장
+
+      history.replace(Uri.home);
+    } catch (e) {
+      if (loginError[e.response.status]) {
+        setError(loginError[e.response.status]);
+        throw e;
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const logout = () => {
     setUserState(null);
@@ -14,8 +45,10 @@ const useAuth = (): { authorize: (user: User) => void; logout: () => void } => {
   };
 
   return {
-    authorize,
+    login,
     logout,
+    isLoading,
+    error,
   };
 };
 
