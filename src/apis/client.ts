5bf17e53475a8db9b6f0443d6597af4df09cfeee
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-03 11:39:54 +0900

    [#10] refactor: api url 환경변수에 대해서 'APP_' prefix 추가

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const apiBaseURL =
   process.env.NODE_ENV === 'development'
-    ? `${process.env.API_URL}/`
-    : process.env.API_URL;
+    ? `${process.env.APP_API_URL}/`
+    : process.env.APP_API_URL;
 
 const client = axios.create();
 client.defaults.baseURL = apiBaseURL;
