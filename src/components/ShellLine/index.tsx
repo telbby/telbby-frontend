1450ae1a33f08f6cf711d18a9ea98166c29e698f
박종호 <jong951005@gmail.com>
2021-11-14 23:03:59 +0900

    [#9] chore: ShellLine Component print line, readline으로 구분하도록 수정

--- src/components/ShellLine/index.tsx
+++ src/components/ShellLine/index.tsx
@@ -23,51 +23,67 @@ export type ShellLineProps = {
   disabled?: boolean;
 };
 
+const ShellReadLine = ({ type, message, disabled }: ShellLineProps) => {
+  const isPassword = message === 'password';
+  return (
+    <>
+      <span css={DefaultTypeStyle}>{type} </span>
+      <label htmlFor="readline" css={DefaultMessageStyle}>
+        {`${message}: `}
+      </label>
+      <input
+        type={isPassword ? 'password' : 'text'}
+        css={InputStyle}
+        maxLength={MAX_SHELL_INPUT_LENGTH}
+        disabled={disabled}
+        autoComplete="off"
+        id="readline"
+      />
+    </>
+  );
+};
+
+const ShellPrintLine = ({ type, message, disabled }: ShellLineProps) => {
+  const isError = type === ShellLineType.Error;
+  return (
+    <>
+      {isError && <span css={ErrorTypeStyle}>ERROR: </span>}
+      <label
+        htmlFor="printline"
+        css={isError ? ErrorMessageStyle : DefaultMessageStyle}
+      >
+        {message}
+      </label>
+      {!isError && (
+        <input
+          type="text"
+          css={InputStyle}
+          maxLength={0}
+          disabled={disabled}
+          id="printline"
+        />
+      )}
+    </>
+  );
+};
+
 const ShellLine = ({
   type,
   message,
   disabled,
 }: ShellLineProps): ReactElement => {
-  const isPassword = message === 'password';
   const isReadLine = [ShellLineType.Question, ShellLineType.Config].includes(
     type,
   );
-  const isError = type === ShellLineType.Error;
 
   return (
-    <label htmlFor={message}>
+    <div>
       {isReadLine ? (
-        <>
-          <span css={DefaultTypeStyle}>{type} </span>
-          <span css={DefaultMessageStyle}>{message}: </span>
-          <input
-            type={isPassword ? 'password' : 'text'}
-            css={InputStyle}
-            maxLength={MAX_SHELL_INPUT_LENGTH}
-            disabled={disabled}
-            data-testid="shell-line-input"
-            autoComplete="off"
-            alt="input"
-          />
-        </>
+        <ShellReadLine type={type} message={message} disabled={disabled} />
       ) : (
-        <>
-          {isError && <span css={ErrorTypeStyle}>ERROR: </span>}
-          <span css={isError ? ErrorMessageStyle : DefaultMessageStyle}>
-            {message}
-          </span>
-          {!isError && (
-            <input
-              type="text"
-              css={InputStyle}
-              maxLength={0}
-              disabled={disabled}
-              data-testid="shell-line-input"
-            />
-          )}
-        </>
+        <ShellPrintLine type={type} message={message} disabled={disabled} />
       )}
-    </label>
+    </div>
   );
 };
 
