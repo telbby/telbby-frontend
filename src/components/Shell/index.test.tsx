6b81c1a8fec1e9782304ebebccccf322272fcfa5
박종호 <jong951005@gmail.com>
2021-11-15 10:10:34 +0900

    [#18] test: Shell Component 테스트 코드 userEvent 사용하도록 수정

--- src/components/Shell/index.test.tsx
+++ src/components/Shell/index.test.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { SHELL_FORM_ELEMENT } from '@/constants/shell';
 
 import Shell from '.';
 
-describe('Shell Component', () => {
+describe('Shell Component 테스트', () => {
+  const shellType = 'login';
+  const requestWhenQueryDone = () =>
+    new Promise((resolve) => setTimeout(() => resolve('Success')));
+  const ShellElement = (
+    <Shell
+      type={shellType}
+      requestWhenQueryDone={requestWhenQueryDone}
+      width={789}
+      height={208}
+    />
+  );
+
   it(`Shell Component는 처음 렌더링 시
     props에 전달되는 type에 해당하는 form element중 첫번째 요소를 렌더링합니다.
     그러나, 두번째 요소는 렌더링하지 않습니다.
   `, () => {
-    const shellType = 'login';
-    const requestWhenQueryDone = () =>
-      new Promise((resolve) => setTimeout(() => resolve('Success')));
-    const { container } = render(
-      <Shell
-        type={shellType}
-        requestWhenQueryDone={requestWhenQueryDone}
-        width={789}
-        height={208}
-      />,
-    );
+    const { container } = render(ShellElement);
 
     const formElement = SHELL_FORM_ELEMENT[shellType];
     const { type: firstLineType, message: firstLineContent } = formElement[0];
@@ -34,21 +38,10 @@ describe('Shell Component', () => {
     );
   });
   it(`Shell Component에 Enter를 입력하면, 다음 요소를 렌더링합니다.`, () => {
-    const shellType = 'login';
-    const requestWhenQueryDone = async () =>
-      new Promise((resolve) => setTimeout(() => resolve('Success')));
-    const { container } = render(
-      <Shell
-        type={shellType}
-        requestWhenQueryDone={requestWhenQueryDone}
-        width={789}
-        height={208}
-      />,
-    );
+    const { container } = render(ShellElement);
 
     const formElement = SHELL_FORM_ELEMENT[shellType];
     const { type: secondLineType, message: secondLineContent } = formElement[1];
-
     expect(container).not.toHaveTextContent(
       `${secondLineType} ${secondLineContent}:`,
     );
@@ -56,11 +49,7 @@ describe('Shell Component', () => {
     const inputs = screen.getAllByRole('textbox');
     const lastInputElement = inputs[inputs.length - 1];
 
-    fireEvent.keyPress(lastInputElement, {
-      key: 'Enter',
-      code: 'Enter',
-      charCode: 13,
-    });
+    userEvent.type(lastInputElement, 'sampleId{enter}');
 
     expect(container).toHaveTextContent(
       `${secondLineType} ${secondLineContent}:`,
