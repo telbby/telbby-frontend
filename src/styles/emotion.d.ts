f8a884690f63be4fe021fd62b2a0a6898738ee23
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-15 11:12:04 +0900

    [#16] feat: emotion theme 객체에 primary light & indigo color 추가

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -4,12 +4,14 @@ declare module '@emotion/react' {
   export interface Theme {
     colorPrimary: string;
     colorPrimaryDark: string;
+    colorPrimaryLight: string;
     colorSub: string;
     colorBg: string;
     colorWhite: string;
     colorGray1: string;
     colorGray2: string;
     colorGray3: string;
+    colorIndigo: string;
     colorBrown: string;
     colorSuccess: string;
     colorError: string;
