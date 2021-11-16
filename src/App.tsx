11dbc8763d5acf546ace9c76b7e50a85ce6ae33a
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-16 19:24:34 +0900

    [#19] feat: App에 confirm modal 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import Uri from './constants/uri';
 import CountPage from './pages/CountPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import ConfirmModalRoot from './components/modal/ConfirmModalRoot';
 
 const App: FC = () => {
   return (
-    <Switch>
-      <Route path={Uri.home} exact component={HomePage} />
-      <Route path={Uri.count} exact component={CountPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <>
+      <Switch>
+        <Route path={Uri.home} exact component={HomePage} />
+        <Route path={Uri.count} exact component={CountPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <ConfirmModalRoot />
+    </>
   );
 };
 
