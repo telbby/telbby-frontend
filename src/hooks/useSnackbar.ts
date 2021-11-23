be3da7987f95aff097caeda7c5be1ef49e155251
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-23 18:45:57 +0900

    [#24] feat: useSnackbar hook 추가

---
+++ src/hooks/useSnackbar.ts
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
+import { SnackbarStateType, useSnackbarState } from '@/atoms/snackbarState';
+
+const SNACKBAR_ANIMATION_DURATION = 600;
+const DEFAULT_SNACKBAR_DURATION = 100;
+
+type ShowSnackbarType = Partial<Omit<SnackbarStateType, 'isOpen'>> & {
+  duration?: number;
+};
+
+const useSnackbar = (
+  initialConfig?: ShowSnackbarType,
+): {
+  showMessage: (message: string, config: ShowSnackbarType) => void;
+  setConfig: (config: ShowSnackbarType) => void;
+} => {
+  const [timerDuration, setTimerDuration] = useState(
+    initialConfig.duration ?? DEFAULT_SNACKBAR_DURATION,
+  );
+  const [snackbarState, setSnackbarState] = useSnackbarState();
+  const timer = useRef(null);
+
+  const setConfig = (config: Partial<SnackbarStateType>) =>
+    setSnackbarState((prev) => ({ ...prev, ...config }));
+
+  const showMessage = (message: string, config: ShowSnackbarType) => {
+    if (config.duration >= 0) {
+      setTimerDuration(config.duration);
+    }
+    setConfig({ ...config, message, isOpen: true });
+  };
+
+  useEffect(() => {
+    if (timer.current) clearTimeout(timer.current);
+    if (!snackbarState.isOpen) return;
+
+    timer.current = setTimeout(() => {
+      setConfig({ isOpen: false });
+    }, timerDuration + SNACKBAR_ANIMATION_DURATION);
+  }, [snackbarState]);
+
+  useEffect(() => {
+    if (initialConfig) setConfig(initialConfig);
+
+    return () => {
+      clearTimeout(timer.current);
+      setConfig({ isOpen: false });
+    };
+  }, []);
+
+  return { showMessage, setConfig };
+};
+
+export default useSnackbar;
