ad0937bf7c18ecceed429a83c2ae1a517e3d7e62
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-03 19:57:52 +0900

    [#45] feat: Api 파생 클래스에서 apiUrl를 바로 사용하지 않도록 httpClient wrapping하는 메서드들 추가

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,8 @@
-import { HTTPClient } from '@/utils/httpClient';
+import {
+  HTTPClient,
+  HTTPRequestConfig,
+  HTTPResponse,
+} from '@/utils/httpClient';
 
 abstract class Api {
   protected apiUrl: string;
@@ -8,6 +12,38 @@ abstract class Api {
   constructor(client: HTTPClient, apiUrl: string) {
     this.client = client;
     this.apiUrl = apiUrl;
+  }
+
+  async request<R = unknown, D = unknown>(
+    config: HTTPRequestConfig<D>,
+  ): Promise<HTTPResponse<R>> {
+    return this.client.request<R, D>(config);
+  }
+
+  async get<R = unknown, D = unknown>(
+    config?: HTTPRequestConfig<D>,
+  ): Promise<HTTPResponse<R>> {
+    return this.client.get<R, D>(this.apiUrl, config);
+  }
+
+  async delete<R = unknown, D = unknown>(
+    config?: HTTPRequestConfig<D>,
+  ): Promise<HTTPResponse<R>> {
+    return this.client.delete<R, D>(this.apiUrl, config);
+  }
+
+  async post<R = unknown, D = unknown>(
+    data: D,
+    config?: HTTPRequestConfig<D>,
+  ): Promise<HTTPResponse<R>> {
+    return this.client.post<R, D>(this.apiUrl, data, config);
+  }
+
+  async put<R = unknown, D = unknown>(
+    data: D,
+    config?: HTTPRequestConfig<D>,
+  ): Promise<HTTPResponse<R>> {
+    return this.client.put(this.apiUrl, data, config);
   }
 }
 
