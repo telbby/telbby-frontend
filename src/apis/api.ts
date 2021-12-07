2ded76855dec4478f4a61cd3df537d98403aa322
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-07 10:13:40 +0900

    [#45] refactor: silent refresh를 위해 api 모듈 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,10 +1,17 @@
+import { AuthResponseBody } from '@/types';
 import {
   HTTPClient,
   HTTPRequestConfig,
   HTTPResponse,
 } from '@/utils/httpClient';
 
+interface ApiRequestConfig<D> extends Omit<HTTPRequestConfig<D>, 'url'> {
+  additionalUrl?: string;
+}
+
 abstract class Api {
+  private readonly ACCESS_TOKEN_HEADER = 'Authorization';
+
   private apiUrl: string;
 
   protected client: HTTPClient;
@@ -14,43 +21,78 @@ abstract class Api {
     this.apiUrl = apiUrl;
   }
 
-  async request<R = unknown, D = unknown>(
-    config: HTTPRequestConfig<D>,
+  protected setAccessToken(accessToken: string): void {
+    this.client.setHeader(this.ACCESS_TOKEN_HEADER, `Bearer ${accessToken}`);
+  }
+
+  protected clearAccessToken(): void {
+    this.client.setHeader(this.ACCESS_TOKEN_HEADER, '');
+  }
+
+  private async requestWithSilentRefresh<R = unknown>(
+    request: () => Promise<HTTPResponse<R & Partial<AuthResponseBody>>>,
   ): Promise<HTTPResponse<R>> {
-    return this.client.request<R, D>(config);
+    const response = await request();
+
+    const { access = '', requestAgain = false } = response.data;
+
+    if (access) this.setAccessToken(access);
+    if (!requestAgain) return response;
+
+    return request();
   }
 
   async get<R = unknown, D = unknown>(
-    config?: HTTPRequestConfig<D>,
+    config?: ApiRequestConfig<D>,
   ): Promise<HTTPResponse<R>> {
-    return this.client.get<R, D>(this.apiUrl, config);
+    const url = `${this.apiUrl}${config?.additionalUrl ?? ''}`;
+
+    const request = () => this.client.get<R, D>(url, config);
+
+    return this.requestWithSilentRefresh<R>(request);
   }
 
   async delete<R = unknown, D = unknown>(
-    config?: HTTPRequestConfig<D>,
+    config?: ApiRequestConfig<D>,
   ): Promise<HTTPResponse<R>> {
-    return this.client.delete<R, D>(this.apiUrl, config);
+    const url = `${this.apiUrl}${config?.additionalUrl ?? ''}`;
+
+    const request = () => this.client.delete<R, D>(url, config);
+
+    return this.requestWithSilentRefresh<R>(request);
   }
 
   async post<R = unknown, D = unknown>(
     data: D,
-    config?: HTTPRequestConfig<D>,
+    config?: ApiRequestConfig<D>,
   ): Promise<HTTPResponse<R>> {
-    return this.client.post<R, D>(this.apiUrl, data, config);
+    const url = `${this.apiUrl}${config?.additionalUrl ?? ''}`;
+
+    const request = () => this.client.post<R, D>(url, data, config);
+
+    return this.requestWithSilentRefresh<R>(request);
   }
 
   async put<R = unknown, D = unknown>(
     data: D,
-    config?: HTTPRequestConfig<D>,
+    config?: ApiRequestConfig<D>,
   ): Promise<HTTPResponse<R>> {
-    return this.client.put<R, D>(this.apiUrl, data, config);
+    const url = `${this.apiUrl}${config?.additionalUrl ?? ''}`;
+
+    const request = () => this.client.put<R, D>(url, data, config);
+
+    return this.requestWithSilentRefresh<R>(request);
   }
 
   async patch<R = unknown, D = unknown>(
     data: Partial<D>,
-    config?: HTTPRequestConfig<D>,
+    config?: ApiRequestConfig<D>,
   ): Promise<HTTPResponse<R>> {
-    return this.client.patch<R, D>(this.apiUrl, data, config);
+    const url = `${this.apiUrl}${config?.additionalUrl ?? ''}`;
+
+    const request = () => this.client.patch<R, D>(url, data, config);
+
+    return this.requestWithSilentRefresh<R>(request);
   }
 }
 
--- src/apis/authApi/index.ts
+++ src/apis/authApi/index.ts
@@ -3,16 +3,6 @@ import { AuthResponseBody, LoginRequestBody } from '@/types';
 import Api from '../api';
 
 class AuthApi extends Api {
-  private readonly ACCESS_TOKEN_HEADER = 'Authorization';
-
-  private setAccessToken(accessToken: string): void {
-    this.client.setHeader(this.ACCESS_TOKEN_HEADER, `Bearer ${accessToken}`);
-  }
-
-  private clearAccessToken(): void {
-    this.client.setHeader(this.ACCESS_TOKEN_HEADER, '');
-  }
-
   async login(reqData: LoginRequestBody): Promise<AuthResponseBody> {
     const response = await this.post<AuthResponseBody, LoginRequestBody>(
       reqData,
