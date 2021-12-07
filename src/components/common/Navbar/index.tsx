ddab8c0952cf18c29b700ee0a53d88e95a4c2ea5
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-07 20:24:37 +0900

    [#49] feat: 로그인 상태에 따른 다른 nav bar 렌더링

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -3,26 +3,54 @@ import { Link } from 'react-router-dom';
 
 import Logo from '../Logo';
 import Uri from '@/constants/uri';
+import { useUserState } from '@/atoms/userState';
 
-import { navStyle, yellowLineButton, yellowFullButton } from './style';
+import {
+  navStyle,
+  yellowLineButtonStyle,
+  yellowFullButtonStyle,
+} from './style';
+import useConfirmModal from '@/hooks/useConfirmModal';
 
 const Navbar: FC = () => {
+  const [user, setUser] = useUserState();
+  const [open] = useConfirmModal();
+
+  const onLogout = () => {
+    const logout = () => setUser(null);
+    open({ message: 'Do you want to logout?', acceptHandler: logout });
+  };
+
   return (
     <nav css={navStyle}>
       <Link to={Uri.home}>
         <Logo />
       </Link>
+
       <div>
-        <Link to={Uri.signin}>
-          <button css={yellowLineButton} type="button">
-            Sign in
-          </button>
-        </Link>
-        <Link to={Uri.signup}>
-          <button css={yellowFullButton} type="button">
-            Register
-          </button>
-        </Link>
+        {!user ? (
+          <>
+            <Link to={Uri.signin}>
+              <span css={yellowLineButtonStyle}>Sign in</span>
+            </Link>
+            <Link to={Uri.signup}>
+              <span css={yellowFullButtonStyle}>Register</span>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to={Uri.service}>
+              <span css={yellowLineButtonStyle}>Service</span>
+            </Link>
+            <button
+              css={yellowFullButtonStyle}
+              type="button"
+              onClick={onLogout}
+            >
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
