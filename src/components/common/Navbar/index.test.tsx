900b4aa639433a0b963a7b8748968b930ffd66c7
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-08 08:28:47 +0900

    [#49] test: 로그인 여부에 따른 테스트 케이스 구분 추가

--- src/components/common/Navbar/index.test.tsx
+++ src/components/common/Navbar/index.test.tsx
@@ -8,57 +8,64 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Navbar from '.';
 
 describe('<Navbar/> component test', () => {
-  it('Logo를 클릭했을 때 홈으로 이동해야 합니다.', () => {
-    const history = createMemoryHistory();
-    history.replace('/other');
+  describe('로그인이 되지 않았을 경우', () => {
+    it('Logo를 클릭했을 때 홈으로 이동해야 합니다.', () => {
+      const history = createMemoryHistory();
+      history.replace('/other');
 
-    render(
-      <RecoilRoot>
-        <Router history={history}>
-          <Navbar />
-        </Router>
-      </RecoilRoot>,
-    );
+      render(
+        <RecoilRoot>
+          <Router history={history}>
+            <Navbar />
+          </Router>
+        </RecoilRoot>,
+      );
 
-    const linkDom = screen.getByRole('link', { name: 'logo telbby' });
-    fireEvent.click(linkDom);
+      const linkDom = screen.getByRole('link', { name: 'logo telbby' });
+      fireEvent.click(linkDom);
 
-    expect(history.location.pathname).toBe('/');
+      expect(history.location.pathname).toBe('/');
+    });
+
+    it('sign in 링크를 클릭했을 때 sign in 페이지로 이동해야 합니다.', () => {
+      const history = createMemoryHistory();
+      history.replace('/other');
+
+      render(
+        <RecoilRoot>
+          <Router history={history}>
+            <Navbar />
+          </Router>
+        </RecoilRoot>,
+      );
+
+      const linkDom = screen.getByRole('link', { name: 'Sign in' });
+      fireEvent.click(linkDom);
+
+      expect(history.location.pathname).toBe('/signin');
+    });
+
+    it('register 링크를 클릭했을 때 sign up 페이지로 이동해야 합니다.', () => {
+      const history = createMemoryHistory();
+      history.replace('/other');
+
+      render(
+        <RecoilRoot>
+          <Router history={history}>
+            <Navbar />
+          </Router>
+        </RecoilRoot>,
+      );
+
+      const linkDom = screen.getByRole('link', { name: 'Register' });
+      fireEvent.click(linkDom);
+
+      expect(history.location.pathname).toBe('/signup');
+    });
   });
 
-  it('sign in 링크를 클릭했을 때 sign in 페이지로 이동해야 합니다.', () => {
-    const history = createMemoryHistory();
-    history.replace('/other');
-
-    render(
-      <RecoilRoot>
-        <Router history={history}>
-          <Navbar />
-        </Router>
-      </RecoilRoot>,
-    );
-
-    const linkDom = screen.getByRole('link', { name: 'Sign in' });
-    fireEvent.click(linkDom);
-
-    expect(history.location.pathname).toBe('/signin');
-  });
-
-  it('register 링크를 클릭했을 때 sign up 페이지로 이동해야 합니다.', () => {
-    const history = createMemoryHistory();
-    history.replace('/other');
-
-    render(
-      <RecoilRoot>
-        <Router history={history}>
-          <Navbar />
-        </Router>
-      </RecoilRoot>,
-    );
-
-    const linkDom = screen.getByRole('link', { name: 'Register' });
-    fireEvent.click(linkDom);
-
-    expect(history.location.pathname).toBe('/signup');
+  describe('로그인이 되었을 경우', () => {
+    // @TODO 로그인이 되었을 경우에 대한 테스트 케이스를 추가해야 합니다.
+    // NavBar 내에서 Recoil 전역 상태를 사용하고 있어서 어떻게 테스트 케이스를 작성해야 할지 모르겠네요... ㅠㅠ
   });
 });
