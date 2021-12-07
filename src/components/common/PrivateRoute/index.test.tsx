9f161f0f5f950b5d842cf22e79c6941369d25133
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-07 15:00:03 +0900

    [#49] test: MemoryRouter로 대체 가능한 부분에 대해 수정

--- src/components/common/PrivateRoute/index.test.tsx
+++ src/components/common/PrivateRoute/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
 
@@ -43,51 +43,42 @@ describe('<PrivateRoute/> component test', () => {
   });
 
   it('접근 조건이 true 때 접근할 경우 prop으로 받은 컴포넌트를 렌더링해야 합니다.', () => {
-    const history = createMemoryHistory();
-    history.replace('/test');
-
     render(
-      <Router history={history}>
+      <MemoryRouter initialEntries={['/test']}>
         <PrivateRoute
           path="/test"
           component={() => <h1>Hello</h1>}
           isAccessible
         />
-      </Router>,
+      </MemoryRouter>,
     );
 
     expect(screen.queryByText('Hello')).toBeInTheDocument();
   });
 
   it('접근 조건이 false 때 접근할 경우 prop으로 받은 컴포넌트를 렌더링하지 않아야 합니다.', () => {
-    const history = createMemoryHistory();
-    history.replace('/test');
-
     render(
-      <Router history={history}>
+      <MemoryRouter initialEntries={['/test']}>
         <PrivateRoute
           path="/test"
           component={() => <h1>Hello</h1>}
           isAccessible={false}
         />
-      </Router>,
+      </MemoryRouter>,
     );
 
     expect(screen.queryByText('Hello')).toBeNull();
   });
 
   it('다른 URL로 접근할 경우 prop으로 받은 컴포넌트를 렌더링하지 않아야 합니다.', () => {
-    const history = createMemoryHistory();
-    history.replace('/other');
-
     render(
-      <Router history={history}>
+      <MemoryRouter initialEntries={['/other']}>
         <PrivateRoute
           path="/test"
           component={() => <h1>Hello</h1>}
           isAccessible
         />
-      </Router>,
+      </MemoryRouter>,
     );
 
     expect(screen.queryByText('Hello')).toBeNull();
