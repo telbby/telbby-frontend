5445178297ffd59577709a3ff05ad5a1c557d6c3
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-07 20:17:35 +0900

    [#49] refactor: nav 스타일 중복 제거

--- src/components/common/Navbar/style.ts
+++ src/components/common/Navbar/style.ts
@@ -7,25 +7,31 @@ export const navStyle = css`
   padding: 20px 12%;
 `;
 
-export const yellowLineButton = (theme: Theme): SerializedStyles => css`
+export const navButtonStyle = (theme: Theme): SerializedStyles => css`
   padding: 7px 16px;
   font-family: ${theme.fontCodingBold};
   font-size: 15px;
-  border: 1px solid ${theme.colorSub};
   border-radius: 6px;
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.15);
+  border: 1px solid ${theme.colorSub};
+  transition: 0.4s;
+
+  :hover {
+    box-shadow: none;
+  }
+`;
+
+export const yellowLineButtonStyle = (theme: Theme): SerializedStyles => css`
   background: transparent;
   color: ${theme.colorSub};
   margin-right: 10px;
+
+  ${navButtonStyle(theme)};
 `;
 
-export const yellowFullButton = (theme: Theme): SerializedStyles => css`
-  padding: 7px 16px;
-  font-family: ${theme.fontCodingBold};
-  font-size: 15px;
-  border: 1px solid ${theme.colorSub};
-  border-radius: 6px;
-  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.15);
+export const yellowFullButtonStyle = (theme: Theme): SerializedStyles => css`
   background-color: ${theme.colorSub};
   color: ${theme.colorWhite};
+
+  ${navButtonStyle(theme)};
 `;
