c584500311a99c1f24096495c857dc7864f48d04
Jongho Park <jong951005@gmail.com>
2021-12-14 02:40:54 +0900

    [#46] chore: resolve, reject 헬퍼 함수 추가

--- src/components/shell/utils.ts
+++ src/components/shell/utils.ts
@@ -15,3 +15,36 @@ export const renderProps: RenderPropsType = (type, prefix, message) => ({
   prefix,
   message,
 });
+
+type ResolveType = (nextSequence: number) => {
+  status: 'success';
+  nextSequence: number;
+};
+
+export const resolve: ResolveType = (nextSequence) => ({
+  status: 'success',
+  nextSequence,
+});
+
+type RejectType = (
+  goBackSequence: number,
+  errorMessage?: string,
+) => {
+  status: 'error';
+  nextSequence: number;
+  render: {
+    type: 'printLine';
+    prefix: 'error';
+    message: string;
+  };
+};
+
+export const reject: RejectType = (goBackSequence, errorMessage) => ({
+  status: 'error',
+  nextSequence: goBackSequence,
+  render: {
+    type: 'printLine',
+    prefix: 'error',
+    message: errorMessage,
+  },
+});
--- src/pages/SigninPage/index.tsx
+++ src/pages/SigninPage/index.tsx
@@ -6,7 +6,7 @@ import Jumbotron from '@/components/common/Jumbotron';
 import Logo from '@/components/common/Logo';
 import ShellCommand from '@/components/shell/ShellCommand';
 import Shell from '@/components/shell/ShellCopy';
-import { renderProps } from '@/components/shell/utils';
+import { reject, renderProps, resolve } from '@/components/shell/utils';
 import { NETWORK_ERROR, UNEXPECTED_ERROR, loginError } from '@/constants/error';
 import Uri from '@/constants/uri';
 import { LoginRequestBody } from '@/types';
@@ -36,18 +36,8 @@ const SigninPage: FC = () => {
             formKey="userId"
             maxLength={30}
             onEnter={async (val) => {
-              if (!val.length) {
-                return {
-                  status: 'error',
-                  nextSequence: 1,
-                  render: renderProps(
-                    'printLine',
-                    'error',
-                    'Please enter your ID',
-                  ),
-                };
-              }
-              return { status: 'success', nextSequence: 2 };
+              if (!val.length) return reject(1, 'Please enter your ID');
+              return resolve(2);
             }}
           />
           <ShellCommand
@@ -57,18 +47,8 @@ const SigninPage: FC = () => {
             maxLength={35}
             render={renderProps('readLine', 'question', 'password')}
             onEnter={async (val) => {
-              if (!val.length) {
-                return {
-                  status: 'error',
-                  nextSequence: 2,
-                  render: renderProps(
-                    'printLine',
-                    'error',
-                    'Please enter your Password',
-                  ),
-                };
-              }
-              return { status: 'success', nextSequence: 3 };
+              if (!val.length) return reject(2, 'Please enter your Password');
+              return resolve(3);
             }}
           />
           <ShellCommand
@@ -78,45 +58,19 @@ const SigninPage: FC = () => {
             maxLength={1}
             render={renderProps('readLine', 'question', 'Sign in? [y/n]')}
             onEnter={async (val, body: LoginRequestBody) => {
-              if (val !== 'y') {
-                return {
-                  status: 'error',
-                  nextSequence: 1,
-                  render: renderProps('printLine', 'error', 'Access denied'),
-                };
-              }
+              if (val !== 'y') return reject(2, 'Access denied');
 
               try {
                 await authApi.login(body);
                 push(Uri.service);
-                return { status: 'success', nextSequence: 4 };
+                return resolve(4);
               } catch (error) {
-                if (!error.response) {
-                  return {
-                    status: 'error',
-                    nextSequence: 1,
-                    render: renderProps('printLine', 'error', NETWORK_ERROR),
-                  };
-                }
+                if (!error.response) return reject(1, NETWORK_ERROR);
 
                 const { status } = error.response;
-                if (loginError[status]) {
-                  return {
-                    status: 'error',
-                    nextSequence: 1,
-                    render: renderProps(
-                      'printLine',
-                      'error',
-                      loginError[status],
-                    ),
-                  };
-                }
+                if (loginError[status]) return reject(1, loginError[status]);
 
-                return {
-                  status: 'error',
-                  nextSequence: 1,
-                  render: renderProps('printLine', 'error', UNEXPECTED_ERROR),
-                };
+                return reject(1, UNEXPECTED_ERROR);
               }
             }}
           />
@@ -132,107 +86,3 @@ const SigninPage: FC = () => {
 };
 
 export default SigninPage;
-
-// const signinScript: ScriptCommand[] = [
-//   {
-//     sequence: 0,
-//     nextSequence: 1,
-//     prefix: '',
-//     message: 'telbby init v0.1.0',
-//     type: 'printLine',
-//   },
-//   {
-//     type: 'readLine',
-//     prefix: 'question',
-//     message: 'username',
-//     sequence: 1,
-// enterHandler: async (val) => {
-//   if (!val.length) {
-//     return {
-//       status: 'error',
-//       type: 'printLine',
-//       prefix: 'error',
-//       message: 'Please enter your ID',
-//       nextSequence: 1,
-//     };
-//   }
-//   return { status: 'success', nextSequence: 2 };
-// },
-//     formKey: 'userId',
-//     maxLength: 30,
-//   },
-//   {
-//     type: 'readLine',
-//     prefix: 'question',
-//     message: 'password',
-//     sequence: 2,
-// enterHandler: async (val) => {
-//   if (!val.length) {
-//     return {
-//       status: 'error',
-//       type: 'printLine',
-//       prefix: 'error',
-//       message: 'Please enter your Password',
-//       nextSequence: 2,
-//     };
-//   }
-//   return { status: 'success', nextSequence: 3 };
-// },
-//     formKey: 'password',
-//     maxLength: 35,
-//   },
-//   {
-//     type: 'readLine',
-//     prefix: 'question',
-//     message: 'Sign in? [y/n]',
-//     sequence: 3,
-//     enterHandler: async (val, body: LoginRequestBody) => {
-//       if (val !== 'y') {
-//         return {
-//           status: 'error',
-//           type: 'printLine',
-//           prefix: 'error',
-//           message: 'Access denied',
-//           nextSequence: 1,
-//         };
-//       }
-
-//       try {
-//         await authApi.login(body);
-//         push(Uri.service);
-//         return { status: 'success', nextSequence: 4 };
-//       } catch (error) {
-//         if (!error.response) {
-//           return {
-//             status: 'error',
-//             type: 'printLine',
-//             prefix: 'error',
-//             message: NETWORK_ERROR,
-//             nextSequence: 1,
-//           };
-//         }
-
-//         const { status } = error.response;
-//         if (loginError[status]) {
-//           return {
-//             status: 'error',
-//             type: 'printLine',
-//             prefix: 'error',
-//             message: loginError[status],
-//             nextSequence: 1,
-//           };
-//         }
-
-//         return {
-//           status: 'error',
-//           type: 'printLine',
-//           prefix: 'error',
-//           message: UNEXPECTED_ERROR,
-//           nextSequence: 1,
-//         };
-//       }
-//     },
-//     defaultValue: 'y',
-//     maxLength: 1,
-//   },
-// ];
