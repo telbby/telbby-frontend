50579f3919a0455324249efc364401f156bbfc56
Jongho Park <jong951005@gmail.com>
2021-12-17 20:43:15 +0900

    [#33] fix: signin, signup 페이지 테스트 코드 수정

--- src/pages/SigninPage/index.test.tsx
+++ src/pages/SigninPage/index.test.tsx
@@ -1,7 +1,9 @@
+import { createMemoryHistory } from 'history';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import SigninPage from '.';
 
@@ -12,21 +14,31 @@ describe('SigninPage 테스트', () => {
     </MemoryRouter>
   );
 
-  it(`로그인 문구를 렌더링해야 합니다.`, () => {
+  it('로그인 문구를 렌더링해야 합니다.', () => {
     render(signupPageContainer);
     expect(screen.getByRole('heading')).toHaveTextContent('Sign in to telbby');
   });
 
-  it(`로고를 렌더링해야 합니다.`, () => {
+  it('로고를 렌더링해야 합니다.', () => {
     render(signupPageContainer);
     expect(screen.getByAltText('logo'));
   });
 
-  // FIXME: #23 이 머지된 이후 테스트 케이스를 수정해야 합니다.
-  //    - href 속성 체크 => 실제로 이동이 되는지 테스트
-  // it(`footer에 안내문과 함께 /signup으로 보내는 링크가 있어야 합니다.`, () => {
-  //   render(signupPageContainer);
-  //   expect(screen.queryByText('Don’t have an account?')).toBeInTheDocument();
-  //   expect(screen.getByRole('link')).toHaveAttribute('href', '/signup');
-  // });
+  it('footer에 "Don’t have an account?" 안내문을 렌더링해야 합니다.', () => {
+    render(signupPageContainer);
+    expect(screen.queryByText('Don’t have an account?')).toBeInTheDocument();
+  });
+
+  it('Sign Up 을 클릭하면 회원가입 페이지로 이동해야 합니다.', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={history}>
+        <SigninPage />
+      </Router>,
+    );
+
+    userEvent.click(screen.getByText('Sign Up'));
+    expect(history.location.pathname).toBe('/signup');
+  });
 });
