a72993e4cc088ffe1f635f5914eca029cbbe9c19
jiyong1 <jyong9591@gmail.com>
2021-12-17 21:02:00 +0900

    [#53] fix: snackbar 컴포넌트 prettier 오류 해결

--- src/components/modal/Snackbar/index.tsx
+++ src/components/modal/Snackbar/index.tsx
@@ -13,25 +13,25 @@ export type SnackbarProps = {
   backgroundColor?: string;
 };
 
-const Snackbar: FC<SnackbarProps> & { defaultProps: Partial<SnackbarProps> } =
-  ({ isOpen, message, color, backgroundColor }) => {
-    const [isShowing, setIsShowing] = useState(false);
-    const visibleStyle =
-      isOpen && isShowing ? snackbarShowStyle : snackbarHideStyle;
+const Snackbar: FC<SnackbarProps> & {
+  defaultProps: Partial<SnackbarProps>;
+} = ({ isOpen, message, color, backgroundColor }) => {
+  const [isShowing, setIsShowing] = useState(false);
+  const visibleStyle =
+    isOpen && isShowing ? snackbarShowStyle : snackbarHideStyle;
 
-    useEffect(() => isOpen && setIsShowing(true), [isOpen]);
-    const hideMessageAfterFadeOutAnimation = () =>
-      !isOpen && setIsShowing(false);
+  useEffect(() => isOpen && setIsShowing(true), [isOpen]);
+  const hideMessageAfterFadeOutAnimation = () => !isOpen && setIsShowing(false);
 
-    return (
-      <div
-        css={[snackbarWrapperStyle, visibleStyle, { color, backgroundColor }]}
-        onTransitionEnd={hideMessageAfterFadeOutAnimation}
-      >
-        {isShowing ? message : ''}
-      </div>
-    );
-  };
+  return (
+    <div
+      css={[snackbarWrapperStyle, visibleStyle, { color, backgroundColor }]}
+      onTransitionEnd={hideMessageAfterFadeOutAnimation}
+    >
+      {isShowing ? message : ''}
+    </div>
+  );
+};
 
 Snackbar.defaultProps = {
   isOpen: false,
