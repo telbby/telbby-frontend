d891a2a6e7b060bc92b3c25e6860478e8a282cc3
Yunseo Hwang <yunseo.dev@gmail.com>
2021-12-31 16:38:39 +0900

    [#67] feat: IconTabNavbar 추가

---
+++ src/components/nav/IconTabNavbar/index.tsx
@@ -0,0 +1,113 @@
+import { Theme } from '@emotion/react';
+import React, { FC, MouseEventHandler } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import {
+  tabNavbarStyle,
+  tabIconStyle,
+  tabItemStyle,
+  tabCurvedBlockStyle,
+} from './style';
+
+type TabItemProps = {
+  to?: string;
+  onClickHandler?: MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode | ((selected: boolean) => React.ReactNode);
+};
+
+const TabItem: FC<TabItemProps> = ({ children, to, onClickHandler }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const onClickTabItem: MouseEventHandler<HTMLButtonElement> = (e) => {
+    if (to) {
+      history.push(to);
+    }
+    if (onClickHandler) {
+      onClickHandler(e);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className="tab-item"
+      css={tabItemStyle}
+      onClick={onClickTabItem}
+    >
+      {typeof children === 'function'
+        ? children(to === location.pathname)
+        : children}
+    </button>
+  );
+};
+
+TabItem.defaultProps = {
+  to: null,
+  onClickHandler: null,
+};
+
+type TabIconProps = {
+  src: string;
+  children?: string;
+  to: string;
+  selectable?: boolean;
+};
+
+const TabIcon: FC<TabIconProps> = ({ children, src, to, selectable }) => {
+  return (
+    <TabItem to={to}>
+      {(selected) => (
+        <div
+          css={tabCurvedBlockStyle}
+          className={`curved-block-wrap ${
+            !selected || !selectable ? 'not-selected' : ''
+          }`}
+        >
+          <div className="curved-block start" />
+          <div
+            css={tabIconStyle}
+            className={`tab-icon ${selected && selectable ? 'selected' : ''}`}
+          >
+            <img src={src} alt={children} />
+          </div>
+          <div className="curved-block end" />
+        </div>
+      )}
+    </TabItem>
+  );
+};
+
+TabIcon.defaultProps = {
+  children: 'icon',
+  selectable: true,
+};
+
+type IconTabNavbarProps = {
+  direction?: 'top' | 'right' | 'bottom' | 'left';
+};
+
+const IconTabNavbar: FC<IconTabNavbarProps> & {
+  TabIcon: FC<TabIconProps>;
+  TabItem: FC<TabItemProps>;
+} = ({ children, direction }) => {
+  const isRow = direction === 'top' || direction === 'bottom';
+
+  return (
+    <div
+      className={direction}
+      css={(cssTheme: Theme) => tabNavbarStyle(cssTheme, isRow)}
+    >
+      {children}
+    </div>
+  );
+};
+
+IconTabNavbar.defaultProps = {
+  direction: 'right',
+};
+
+IconTabNavbar.TabIcon = TabIcon;
+IconTabNavbar.TabItem = TabItem;
+
+export default IconTabNavbar;
